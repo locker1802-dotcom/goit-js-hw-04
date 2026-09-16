7b4ea9903948ea94b0f1308b9f26f5abda57fb0e
Dmytro Tsyukh <Locker1802@gmail.com>
2026-09-16 13:42:53 +0300

    v2

--- js/task-2.js
+++ js/task-2.js
@@ -4,7 +4,11 @@ function calcAverageCalories(days) {
     return 0;
   }
 
-  const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
+  let totalCalories = 0;
+  for (let i = 0; i < days.length; i++) {
+    totalCalories += days[i].calories;
+  }
+
   return totalCalories / days.length;
 }
 console.log(
